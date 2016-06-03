4f5ec012a6c1302b13e95bff31272ddd3c25829c
tobiasflohre <tobias.flohre@codecentric.de>
2016-06-03 18:38:39 +0200

    Update to Brixton.RELEASE

--- src/main/java/de/codecentric/EdmpSampleStreamSinkApplication.java
+++ src/main/java/de/codecentric/EdmpSampleStreamSinkApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.integration.annotation.ServiceActivator;
 
 @SpringBootApplication
 @EnableBinding(Sink.class)
